63a723bd899383357ce531df8b129aef707fe84e
Hoàng Việt Hùng <hoangviet.hung.009@gmail.com>
2022-06-03 13:35:41 +0700

    done homepage

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,16 +1,40 @@
 const initSliderPackages = () => {
-    let packagesSlider = new Swiper(".packages-index .swiper-container", {
+    const swiperPackages = new Swiper(".packages-swiper .swiper-container", {
         slidesPerView: 4,
-        speed: 2000,
-        spaceBetween: 30,
+        speed: 1000,
+        spaceBetween: 22,
         autoplay: {
             delay: 2000,
+        },
+        navigation: {
+            nextEl: ".packages-swiper .swiper-button-next",
+            prevEl: ".packages-swiper .swiper-button-prev",
+        },
+        pagination: {
+            el: ".packages-swiper .swiper-pagination",
+            dynamicBullets: true,
         },
         breakpoints: {},
     });
 };
 
+const initSliderScreenShot = () => {
+    const swiperScreenShot = new Swiper(
+        ".screenshot-swiper .swiper-container",
+        {
+            slidesPerView: 1,
+            speed: 2000,
+            autoplay: {
+                delay: 2000,
+            },
+            centeredSlides: true,
+            loop: true,
+        },
+    );
+};
+
 (function ($) {
     "use strict";
     initSliderPackages();
+    initSliderScreenShot();
 })(jQuery, window);
