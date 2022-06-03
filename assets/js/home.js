5da1b35a37daea7877ae0d680746677cf05f0e68
Hoàng Việt Hùng <hoangviet.hung.009@gmail.com>
2022-06-03 15:11:23 +0700

    done animation home

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,40 +1,56 @@
-const initSliderPackages = () => {
-    const swiperPackages = new Swiper(".packages-swiper .swiper-container", {
-        slidesPerView: 4,
-        speed: 1000,
-        spaceBetween: 22,
-        autoplay: {
-            delay: 2000,
-        },
-        navigation: {
-            nextEl: ".packages-swiper .swiper-button-next",
-            prevEl: ".packages-swiper .swiper-button-prev",
-        },
-        pagination: {
-            el: ".packages-swiper .swiper-pagination",
-            dynamicBullets: true,
-        },
-        breakpoints: {},
-    });
-};
-
-const initSliderScreenShot = () => {
-    const swiperScreenShot = new Swiper(
-        ".screenshot-swiper .swiper-container",
-        {
-            slidesPerView: 1,
-            speed: 2000,
-            autoplay: {
-                delay: 2000,
-            },
-            centeredSlides: true,
-            loop: true,
-        },
-    );
-};
-
 (function ($) {
     "use strict";
-    initSliderPackages();
-    initSliderScreenShot();
+    const imJs = {
+        m: function (e) {
+            imJs.d();
+            imJs.methods();
+        },
+        d: function (e) {
+            (this._window = $(window)),
+                (this._document = $(document)),
+                (this._body = $("body")),
+                (this._html = $("html"));
+        },
+        methods: function (e) {
+            imJs.initSliderPackages();
+            imJs.initSliderScreenShot();
+        },
+        initSliderPackages() {
+            const swiperPackages = new Swiper(
+                ".packages-swiper .swiper-container",
+                {
+                    slidesPerView: 4,
+                    speed: 1000,
+                    spaceBetween: 22,
+                    autoplay: {
+                        delay: 2000,
+                    },
+                    navigation: {
+                        nextEl: ".packages-swiper .swiper-button-next",
+                        prevEl: ".packages-swiper .swiper-button-prev",
+                    },
+                    pagination: {
+                        el: ".packages-swiper .swiper-pagination",
+                        dynamicBullets: true,
+                    },
+                    breakpoints: {},
+                },
+            );
+        },
+        initSliderScreenShot() {
+            const swiperScreenShot = new Swiper(
+                ".screenshot-swiper .swiper-container",
+                {
+                    slidesPerView: 1,
+                    speed: 2000,
+                    autoplay: {
+                        delay: 2000,
+                    },
+                    centeredSlides: true,
+                    loop: true,
+                },
+            );
+        },
+    };
+    imJs.m();
 })(jQuery, window);
